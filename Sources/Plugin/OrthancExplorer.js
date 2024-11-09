1140e1a1c1949e6b42523242664f4bb8f7dd9117
Matthew Jacob <github@feral.com>
2024-11-09 15:12:16 -0800

    Checkpoint.
    + Solid button inside interact box- wrong look, but standout color (blue)
    + Correct, non-cached, data
    + alerts for seeing 0010,0020 and 0008,0050

--- Sources/Plugin/OrthancExplorer.js
+++ Sources/Plugin/OrthancExplorer.js
@@ -15,28 +15,91 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  **/
+var resourceId;
 
-function CreateReport(resourceId)
+function CreateReport(rid)
 {
-//  $('#' + 'Reports').remove();
+  resourceId = rid;
+  console.log('In CreateReport: ' + resourceId);
+
+//  var b = $('<a>')
+//      .attr('id', 'kp-report')
+//      .attr('data-role', 'button')
+//      .attr('href', '#')
+//      .attr('data-theme', 'e') // yellow
+//      .attr('data-icon', 'forward')
+//      .text('Create Radiology Report')
+//      .button();
 
   var b = $('<a>')
-      .attr('id', 'Reports')
+      .attr('id', 'kp-report')
       .attr('data-role', 'button')
       .attr('href', '#')
+      .attr('data-theme', 'b') // blue
       .attr('data-icon', 'forward')
-      .attr('data-theme', 'e')
-      .text('Create Radiology Report');
+      .text('Create Radiology Report')
+      .button();
 
-  b.insertBefore($('#series-delete').parent().parent());
-
+  b.insertBefore($('#study-delete').parent().parent());
   b.click(function() {
-    if ($.mobile.pageData) {
-      alert('I have been selected! I am ready to think about templates!');
-    }
+      $.ajax({
+          url: '../studies/' + resourceId + '?full',
+          dataType: 'json',
+          async: false,
+          cache: false,
+          success: function(study) {
+              console.log('In Ajax success: ' + resourceId);
+              console.log(study);
+              var ACCESSION_NUMBER = '0008,0050';
+              var PATIENT_ID = '0010,0020';
+              for (var i in study.MainDicomTags) {
+                  if (i == ACCESSION_NUMBER) {
+                      var val = study.MainDicomTags[i].Value;
+                      alert('Found ACCESSION_NUMBER: ' + val);
+                  }
+              }
+              for (var i in study.PatientMainDicomTags) {
+                  if (i == PATIENT_ID) {
+                      var val = study.PatientMainDicomTags[i].Value;
+                      alert('Found PATIENT_ID: ' + val);
+                  }
+              }
+          },
+          error: function(XMLHttpRequest, textStatus, errorThrown) {
+              console.log('In Ajax error: ' + resourceId);
+              console.log("Status: " + textStatus);
+              console.log("Error: " + errorThrown);
+          }
+      });
   });
 }
 
 $('#study').live('pagebeforeshow', function() {
-CreateReport($.mobile.pageData.uuid);
+    if ($.mobile.pageData) {
+        CreateReport($.mobile.pageData.uuid);
+    }
+});
+$('#patient').live('pagebeforeshow', function() {
+    if ($.mobile.pageData) {
+        console.log('In Patient for ' + $.mobile.pageData.uuid);
+        $('#' + 'kp-report').remove();
+    }
+});
+$('#series').live('pagebeforeshow', function() {
+    if ($.mobile.pageData) {
+        console.log('In Series for ' + $.mobile.pageData.uuid);
+        $('#' + 'kp-report').remove();
+    }
+});
+$('#instance').live('pagebeforeshow', function() {
+    if ($.mobile.pageData) {
+        console.log('In Instance for ' + $.mobile.pageData.uuid);
+        $('#' + 'kp-report').remove();
+    }
+});
+$('#lookup').live('pagebeforeshow', function() {
+    if ($.mobile.pageData) {
+        console.log('In Lookup for ' + $.mobile.pageData.uuid);
+        $('#' + 'kp-report').remove();
+    }
 });
