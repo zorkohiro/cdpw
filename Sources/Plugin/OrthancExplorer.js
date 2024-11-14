3f4606fe4537f08519cec98d5d5f564278b750d6
Matthew Jacob <github@feral.com>
2024-11-14 15:33:33 -0800

    Do some alignment (2 space indenting).
    Set up for making sure we trap bad DICOM study objects before we
    put up a choice menu. We are supposed to able to do another $ajax
    in the .done(functinn() {}); body.

--- Sources/Plugin/OrthancExplorer.js
+++ Sources/Plugin/OrthancExplorer.js
@@ -19,77 +19,82 @@ function CreateReport(resourceId)
 {
   console.log('In CreateReport: ' + resourceId);
   var b = $('<a>')
-      .attr('id', 'kp-report')
-      .attr('data-role', 'button')
-      .attr('href', '#')
-      .attr('data-theme', 'b') // blue
-      .attr('data-icon', 'forward')
-      .text('Create Study Report')
-      .button();
+    .attr('id', 'kp-report')
+    .attr('data-role', 'button')
+    .attr('href', '#')
+    .attr('data-theme', 'b') // blue
+    .attr('data-icon', 'forward')
+    .text('Create Study Report')
+    .button();
 
   b.insertBefore($('#study-delete').parent().parent());
   b.click(function() {
-      $.ajax({
-          url: '../studies/' + resourceId + '?full',
-          dataType: 'json',
-          async: false,
-          cache: false,
-          success: function(study) {
-              var session = "none";
-              var mrn = "none";
-              var ACCESSION_NUMBER = '0008,0050';
-              var PATIENT_ID = '0010,0020';
-              for (var i in study.MainDicomTags) {
-                  if (i == ACCESSION_NUMBER) {
-                      session = study.MainDicomTags[i].Value;
-                      break;
-                  }
-              }
-              for (var i in study.PatientMainDicomTags) {
-                  if (i == PATIENT_ID) {
-                      mrn = study.PatientMainDicomTags[i].Value;
-                      break;
-                  }
-              }
-              console.log(study);
-              if (session != "none" && mrn != "none") {
-                  console.log("Study " + resourceId + ' has ACESSION_NUMBER ' + session + ' and PATIENT_ID ' + mrn);
-              } else if (session == "none" && mrn == "none") {
-                  alert("Study has no ACCESSION_NUMBER or PATIENT_ID defined");
-              } else if (session == "none") {
-                  alert("Study has no ACCESSION_NUMBER defined");
-              } else if (mrn == "none") {
-                  alert("Study has no PATIENT_ID defined");
-              }
+    var session = "none";
+    var mrn = "none";
+    $.ajax({
+      url: '../studies/' + resourceId + '?full',
+      dataType: 'json',
+      async: false,
+      cache: false,
+      success: function(study) {
+        var ACCESSION_NUMBER = '0008,0050';
+        var PATIENT_ID = '0010,0020';
+        for (var i in study.MainDicomTags) {
+          if (i == ACCESSION_NUMBER) {
+            session = study.MainDicomTags[i].Value;
+            break;
           }
-      });
+        }
+        for (var i in study.PatientMainDicomTags) {
+          if (i == PATIENT_ID) {
+            mrn = study.PatientMainDicomTags[i].Value;
+            break;
+          }
+        }
+        console.log(study);
+        if (session != "none" && mrn != "none") {
+          console.log("Study " + resourceId + ' has ACESSION_NUMBER ' + session + ' and PATIENT_ID ' + mrn);
+        } else if (session == "none" && mrn == "none") {
+          alert("Study has no ACCESSION_NUMBER or PATIENT_ID defined");
+        } else if (session == "none") {
+          alert("Study has no ACCESSION_NUMBER defined");
+        } else if (mrn == "none") {
+          alert("Study has no PATIENT_ID defined");
+        }
+      }
+    }).done(function(){
+      console.log("session is " + session + "; mrn is " + mrn);
+      if (session != "none" && mrn != "none") {
+        console.log("will be putting up a template choice box here");
+      }
+    });
   });
 }
 
 $('#study').live('pagebeforeshow', function() {
-    if ($.mobile.pageData) {
-        if ($.mobile.pageData.uuid) {
-            CreateReport($.mobile.pageData.uuid);
-        }
+  if ($.mobile.pageData) {
+    if ($.mobile.pageData.uuid) {
+        CreateReport($.mobile.pageData.uuid);
     }
+  }
 });
 $('#patient').live('pagebeforeshow', function() {
-    if ($.mobile.pageData) {
-        $('#' + 'kp-report').remove();
-    }
+  if ($.mobile.pageData) {
+    $('#' + 'kp-report').remove();
+  }
 });
 $('#series').live('pagebeforeshow', function() {
-    if ($.mobile.pageData) {
-        $('#' + 'kp-report').remove();
-    }
+  if ($.mobile.pageData) {
+    $('#' + 'kp-report').remove();
+  }
 });
 $('#instance').live('pagebeforeshow', function() {
-    if ($.mobile.pageData) {
-        $('#' + 'kp-report').remove();
-    }
+  if ($.mobile.pageData) {
+    $('#' + 'kp-report').remove();
+  }
 });
 $('#lookup').live('pagebeforeshow', function() {
-    if ($.mobile.pageData) {
-        $('#' + 'kp-report').remove();
-    }
+  if ($.mobile.pageData) {
+    $('#' + 'kp-report').remove();
+  }
 });
